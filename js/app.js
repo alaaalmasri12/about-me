7e3e9172f3fe9280df6e3486f6685f45678c3876
alaaalmasri <alaaalmasri272@gmail.com>
2020-02-19 08:16:50 +0200

    last commit

--- js/app.js
+++ js/app.js
@@ -119,62 +119,14 @@
 // }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-var totalQuestions=7;
-var score=0;
-var question1=prompt("Do i like horror movies? ");
-if(question1.toLowerCase()=== 'yes'||question1.toLowerCase()=== 'y'||
- question1.toLowerCase() === 'no' ||question1.toLowerCase()=='n')
-{
-    switch(question1.toLowerCase())
-    {
+var totalQuestions = 7;
+var score = 0;
+var question1 = prompt("Do i like horror movies? ");
+if (question1.toLowerCase() === 'yes' || question1.toLowerCase() === 'y' ||
+    question1.toLowerCase() === 'no' || question1.toLowerCase() == 'n') {
+    switch (question1.toLowerCase()) {
         case 'yes':
         case 'y':
             score++;
@@ -189,48 +141,43 @@ if(question1.toLowerCase()=== 'yes'||question1.toLowerCase()=== 'y'||
 
 }
 
-else
-{
-     console.log('enter either yes or no then repeat the answer');
-     alert("enter either yes or no then repeat the answer");
+else {
+    console.log('enter either yes or no then repeat the answer');
+    alert("enter either yes or no then repeat the answer");
     //choice(question2);
 }
 
-var question2=prompt("Do i like  animes ?");
-if(question2.toLowerCase()=== 'yes'||question2.toLowerCase()=== 'y'||
- question2.toLowerCase() === 'no' ||question2.toLowerCase()=='n')
-{
-    switch(question2)
-    {
+var question2 = prompt("Do i like  animes ?");
+if (question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y' ||
+    question2.toLowerCase() === 'no' || question2.toLowerCase() == 'n') {
+    switch (question2) {
         case 'yes':
         case 'y':
             score++;
             console.log('yes iam an   an otuku');
             alert("yes iam an   an otuku");
             break;
-            case 'no':
-            case 'n':
-                alert("your wrong i am big fan of anime");
-                console.log('your wrong i am big fan of anime');
+        case 'no':
+        case 'n':
+            alert("your wrong i am big fan of anime");
+            console.log('your wrong i am big fan of anime');
 
-                break;
+            break;
 
 
     }
 
 }
 
-else
-{
+else {
     console.log('enter either yes or no then repeat the answer');
     alert("enter either yes or no then repeat the answer");
     //choice2(question2);
 }
-var question3=prompt("do i like pc Consoles");
-if(question3.toLowerCase()=== 'yes'||question3.toLowerCase()=== 'y'||
- question3.toLowerCase() === 'no' ||question3.toLowerCase()=='n'){
-    switch(question3)
-    {
+var question3 = prompt("do i like pc Consoles");
+if (question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y' ||
+    question3.toLowerCase() === 'no' || question3.toLowerCase() == 'n') {
+    switch (question3) {
         case 'yes':
         case 'y':
             score++;
@@ -240,81 +187,76 @@ if(question3.toLowerCase()=== 'yes'||question3.toLowerCase()=== 'y'||
         case 'no':
         case 'n':
             console.log('who doesnt like computer you can do everything on it rethink');
-    alert("who doesnt like computer you can do everything on it rethink");
+            alert("who doesnt like computer you can do everything on it rethink");
 
     }
 
 }
 
-else
-{
+else {
     console.log('enter either yes or no then repeat the answer');
     alert("enter either yes or no then repeat the answer");
     //choice3(question3);
 }
-var question4 =prompt('do i like fourtnite ?');
-if(question4.toLowerCase()=== 'yes'||question4.toLowerCase()=== 'y'||
- question4.toLowerCase() === 'no' ||question4.toLowerCase()=='n'){
-    switch(question4)
-    {
+var question4 = prompt('do i like fourtnite ?');
+if (question4.toLowerCase() === 'yes' || question4.toLowerCase() === 'y' ||
+    question4.toLowerCase() === 'no' || question4.toLowerCase() == 'n') {
+    switch (question4) {
         case 'yes':
         case 'y':
             score++;
             console.log('you guessed right');
             alert("no i hate this game too childish");
             break;
-            case 'no':
-            case 'n':
-                console.log('you guessed right its a kid game.');
-                alert("maybe you think its a kid game");
+        case 'no':
+        case 'n':
+            console.log('you guessed right its a kid game.');
+            alert("maybe you think its a kid game");
 
-                break;
+            break;
     }
 }
 
-else
-{
+else {
     console.log('enter either yes or no then repeat the answer');
     alert("enter either yes or no then repeat the answer");
-   // choice4(question4);
+    // choice4(question4);
 }
-var question5=prompt('am i gona pass 201 course ');
-if(question5.toLowerCase()=== 'yes'||question5.toLowerCase()=== 'y'||
- question5.toLowerCase() === 'no' ||question5.toLowerCase()=='n'){
-    switch(question5)
-    {
+var question5 = prompt('am i gona pass 201 course ');
+if (question5.toLowerCase() === 'yes' || question5.toLowerCase() === 'y' ||
+    question5.toLowerCase() === 'no' || question5.toLowerCase() == 'n') {
+    switch (question5) {
         case 'yes':
         case 'y':
             score++;
             console.log('hell yes im gona pass');
             alert("hell yes im gona pass");
             break;
-            case'no':
-            case 'n':
+        case 'no':
+        case 'n':
             console.log('you are wrong  you will see');
-    alert("you are wrong  you will see");
+            alert("you are wrong  you will see");
 
             break;
     }
 
 }
 
-else
-{
+else {
     console.log('enter either yes or no then repeat the answer');
     alert("enter either yes or no then repeat the answer");
 
 }
-var name=prompt('whats your name?');
-alert("welcome mr."+' '+ name);
+var name = prompt('whats your name?');
+alert("welcome mr." + ' ' + name);
 question1.toLowerCase();
-document.querySelector(".description").innerHTML="Welcome mr"+' '+ name;
+document.querySelector(".description").innerHTML = "Welcome mr" + ' ' + name;
 var rightanswer = 5;
 alert("guess a number in my head");
 var counter = 0;
 
 while (counter < 5) {
-    var question6 = parseInt(prompt("Guess a number?? test ur luck with this number"+Math.floor(Math.random() * 10) + 1));
+    var question6 = parseInt(prompt("Guess a number?? test ur luck with this number" + Math.floor(Math.random() * 10) + 1));
     if (question6 > 5) {
         alert("its too high");
         counter++;
@@ -329,11 +271,11 @@ while (counter < 5) {
         break;
     }
 
-counter++;
-if (counter == 5) {
-    alert("sorry you excdeded ur trys");
+    counter++;
+    if (counter == 5) {
+        alert("sorry you excdeded ur trys");
 
-}
+    }
 }
 
 //question7
@@ -342,7 +284,7 @@ var games = ['dota', 'bioshock', 'plants vs zombies', 'pupg', 'fifa'];
 var gamequestion = prompt("guess my faviorite game").toLowerCase();
 while (counter < 7) {
 
-    console.log(counter);
+    //console.log(counter);
     for (var i = 0; i < games.length; i++) {
         // console.log(i);
         // console.log(counter);
@@ -351,64 +293,77 @@ while (counter < 7) {
         // if (gamequestion === games[i]) {
         //     score++;
         //     alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa');
-            // i = games.length;
-            // counter = 7;
-            // break;
+        // i = games.length;
+        // counter = 7;
+        // break;
         // }
-        switch(gamequestion)
-        {
+        switch (gamequestion) {
             case games[0]:
                 score++;
                 alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
                 i = games.length;
-            counter = 7;
-            break;
-                case games[1]:
-                    score++;
-                alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
-                i = games.length;
-            counter = 7;
-            break;
-                case games[2]:
-                    score++;
+                counter = 7;
+
+
+
+
+                break;
+            case games[1]:
+                score++;
                 alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
                 i = games.length;
                 counter = 7;
                 break;
-                case games[3]:
-                    score++;
-                alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
-                break;
-                case games[4]:
-                    score++;
+            case games[2]:
+                score++;
                 alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
                 i = games.length;
                 counter = 7;
                 break;
-                case games[5]:
-                    score++;
+            case games[3]:
+                score++;
+                alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
+                break;
+            case games[4]:
+                score++;
                 alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
                 i = games.length;
-            counter = 7;
-            break;
-                default:
- gamequestion = prompt("guess my faviorite game");
-            counter++;
+                counter = 7;
+                break;
+            case games[5]:
+                score++;
+                alert("my fav games are " + 'dota bioshcok ,fourtnite,pupg,fifa')
+                i = games.length;
+                counter = 7;
+                break;
+            default:
+                gamequestion = prompt("guess my faviorite game");
+                counter++;
+                console.log(counter);
+                break;
+        }
+
+        if (counter == 6) {
+            alert("you have excedeed your trys");
+            counter=7;
             break;
         }
     }
-        // else {
-        //     alert("guess again");
-        // }
-        // if (counter === 7) {
-        //     alert("you have exceded your trys ");
-        // } else {
-        //     gamequestion = prompt("guess my faviorite game");
-        //     counter++;
-        //     break;
-        // }
-    }
-alert("your score is"+' '+score+ ' '+'out of'+' '+totalQuestions);
+
+    // else {
+    //     alert("guess again");
+    // }
+    // if (counter === 7) {
+    //     alert("you have exceded your trys ");
+    // } else {
+    //     gamequestion = prompt("guess my faviorite game");
+    //     counter++;
+    //     break;
+    // }
+
+}
+
+alert("your score is" + ' ' + score + ' ' + 'out of' + ' ' + totalQuestions);
 // if (counter2 === 7) {
 //     alert("you have exceded your trys ");
 // }
